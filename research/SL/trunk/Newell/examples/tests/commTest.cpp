e3e1185d8c72c4b0698ef16b7cbfca835de30dd1
Donnie Newell <donaldnewell08@gmail.com>
2012-07-07 18:19:16 -0400

    initial add of comm test.

---
+++ research/SL/trunk/Newell/examples/tests/commTest.cpp
@@ -0,0 +1,59 @@
+/* Copyright Donnie Newell 2012 */
+#include <mpi.h>
+#include "../comm.h"
+
+void initializeSubDomain(SubDomain* sub_domain, DTYPE value) {
+  DTYPE* buffer = sub_domain->getBuffer();
+  int height = sub_domain->getLength(0);
+  int width = sub_domain->getLength(1);
+  for (int i = 0; i < height * width; ++i)
+    buffer[i] = value;
+}
+
+void printNeighbors(SubDomain* block, int my_rank) {
+  int* neighbors = block->getNeighbors();
+    for (NeighborTag2D neighbor = x2DNeighborBegin; neighbor < x2DNeighborEnd; ++neighbor)
+      printf("[%d]%s:%d\n", my_rank, neighborString(neighbor), neighbors[neighbor]);
+}
+
+int main(int argc, char** argv) {
+  int my_rank = -1, x_offset = 0, y_offset = 0, length = 3, grid_height = 1,
+          grid_width = 2;
+  int id[2] = {0};
+  int border[3] = {1, 1, 0};
+  int neighbors[26] = {-1};
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  Node node;
+  SubDomain* block = NULL;
+  id[0] = 0;
+  id[1] = 0;
+
+  if (0 == my_rank) {
+    // initialize block 0
+    neighbors[x2DPole1] = 1;
+    printf("rank %d exchanges neighbor %s\n", my_rank, neighborString(x2DPole1));
+    block = new SubDomain(id, x_offset, length + 2 * border[0], y_offset,
+            length + 2 * border[1], grid_height, grid_width, neighbors);
+  } else if (1 == my_rank) {
+    // initialize block 1
+    NeighborTag2D opposite_neighbor = getOppositeNeighbor2D(x2DPole1);
+    printf("rank %d exchanges neighbor %s\n", my_rank, neighborString(opposite_neighbor));
+    neighbors[opposite_neighbor] = 0;
+    id[1] = 1;
+    y_offset += length;
+    block = new SubDomain(id, x_offset, length + 2 * border[0], y_offset,
+            length + 2 * border[1], grid_height, grid_width, neighbors);
+  }
+  printNeighbors(block, my_rank);
+  initializeSubDomain(block, my_rank);
+  node.addSubDomain(block);
+  
+  // perform exchange
+  updateAllStaleData(&node, border);
+
+  delete block;
+  block = NULL;
+  MPI_Finalize();
+  return 0;
+}
